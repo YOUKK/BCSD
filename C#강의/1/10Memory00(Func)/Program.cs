e5816ba511c6747d1af1d7c4b5a97f65a4177aa8
KimYouKyeong <kimyoukeng@naver.com>
2021-05-30 21:02:40 +0900

    10강

---
+++ C#강의/1/10Memory00(Func)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Player
+{
+
+}
+
+class Program
+{
+    // C#은 Main이라는 이름의 함수에서 시작된다.
+    // 이게 메인 함수이다.
+    // static을 빼면 함수의 모양을 가지고 있다.
+    static void Main(string[] args)
+    {
+        Player Newplayer = new Player();
+    }
+}
